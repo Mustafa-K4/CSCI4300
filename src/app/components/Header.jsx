f322a14d5578ff6de35d90a27cc6e1af95aec1a4
PaulOgunmwonyi <paul.ogunmwonyi@gmail.com>
2025-04-09 22:40:29 -0400

    deleted the next.js starter code and created a home page and headbar with routing to blank pages

---
+++ src/app/components/Header.jsx
@@ -0,0 +1,22 @@
+import './Header.css';
+import Link from 'next/link';
+
+export default function Header() {
+
+    return <header className="App-header">
+        <div className="flex items-center justify-center w-1/8">
+            <img src="/logo.jpeg" className="h-[80px] rounded-full shadow-md" alt="logo" />
+        </div>
+        <div className="w-1/4 text-center flex items-center">
+            <h1 className="text-4xl font-bold text-gray-800">Welcome!</h1>
+        </div>
+        <nav className="w-5/8 flex justify-end mr-4">
+            <ul className="flex gap-3 items-center">
+                <li><Link href="/pages/BrowseEvents" className='px-4 py-2 bg-[#E9EAE0] text-[#C85250] rounded-lg hover:bg-[#D8DBCC] transition shadow"'>Browse Events</Link></li>
+                <li><Link href="/pages/CreateEvents" className='px-4 py-2 bg-[#E9EAE0] text-[#C85250] rounded-lg hover:bg-[#D8DBCC] transition shadow"'>Create An Event</Link></li>
+                <li><Link href="/pages/Profile" className="px-4 py-2 bg-[#E9EAE0] text-[#C85250] rounded-lg hover:bg-[#D8DBCC] transition shadow">Profile</Link></li>
+                <li><Link href="/pages/SignIn" className="px-4 py-2 bg-black text-white rounded-lg hover:bg-[#D8DBCC] transition shadow">Sign In</Link></li>
+            </ul>
+        </nav>
+    </header>
+}
---
+++ src/app/pages/Profile/page.jsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function ProfilePage() {
+    return ( 
+        <>
+            <article className="block">
+                <p> Hi!</p>
+            </article>
+    </>
+    );
+}; 
