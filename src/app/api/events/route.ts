96a01864db7aab55764862537ad6f31b7cd5a553
MustafaK4 <MUSTAFA.KHAN.CLASSOF2022@GMAIL.COM>
2025-04-21 15:08:02 -0400

    updated event schema and endpoints

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -5,8 +5,13 @@ import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
     await connectMongoDB("Events");
+    const Events = await Event.find();
+    return NextResponse.json({Events}, {status: 200});
 }
 
 export async function POST(request: NextRequest) {
+    const {owner, name, date, location, description, imageURL, startTime, endTime} = await request.json();
     await connectMongoDB("Events");
+    Event.create({owner, name, date, location, description, imageURL, startTime, endTime});
+    return NextResponse.json({message: "Event created successfully"}, {status: 201});
 }
--- src/models/eventSchema.ts
+++ src/models/eventSchema.ts
@@ -1,6 +1,7 @@
 import mongoose, { Schema, Document, Model } from 'mongoose';
 
 interface Event extends Document {
+    owner: string;
     name: string;
     date: Date;
     location: string;
@@ -11,13 +12,15 @@ interface Event extends Document {
 }
 
 const eventSchema = new Schema<Event>({
+    owner: { type: String, required: true },
     name: { type: String, required: true },
     date: { type: Date, required: true },
     location: { type: String, required: true },
     description: { type: String, required: true },
     imageUrl: { type: String, required: false },
     startTime: { type: String, required: true },
-    endTime: { type: String, required: true }
+    endTime: { type: String, required: false }
+    
 });
 
 const Item: Model<Event> = mongoose.models.Event || mongoose.model<Event>('Event', eventSchema);
