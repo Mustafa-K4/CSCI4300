201ed0b3141611efc4e5dddb71722566819fba19
MustafaK4 <MUSTAFA.KHAN.CLASSOF2022@GMAIL.COM>
2025-04-22 13:52:46 -0400

    added events to user schema and updated token

--- src/models/userSchema.ts
+++ src/models/userSchema.ts
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   lastName:  { type: String, required: true },
   email:     { type: String, required: true, unique: true },
   password:  { type: String, required: true },
+  events:    { type: String, required: false },
 });
 
 export default userSchema;
